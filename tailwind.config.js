58f93ae0534fb17e8f53e531ccf257e72b0f52dc
j-0-n-e-z <j000neeez@gmail.com>
2023-05-15 14:57:33 +0500

    add h1

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+import defaultTheme from 'tailwindcss/defaultTheme'
+
 module.exports = {
 	content: ['./dist/**/*.{html,js}'],
 	theme: {
@@ -7,9 +9,11 @@ module.exports = {
 				lightGray: 'hsl(212, 45%, 89%)',
 				grayishBlue: 'hsl(220, 15%, 55%)',
 				darkBlue: 'hsl(218, 44%, 22%)'
+			},
+			fontFamily: {
+				Outfit: ['Outfit', ...defaultTheme.fontFamily.sans]
 			}
 		}
 	},
 	plugins: []
 }
-
